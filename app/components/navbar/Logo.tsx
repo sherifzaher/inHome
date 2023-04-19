eb11517fca784596e2b396af2d601500cd505404
Sherif <sherifzaher@Sherifs-Macbook.local>
2023-04-19 20:24:50 +0200

    style: update the logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -2,18 +2,22 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 function Logo() {
   const router = useRouter();
   return (
-    <Image
-      onClick={() => router.push('/')}
-      src="/images/logo.png"
-      alt="Logo"
-      className="hidden cursor-pointer md:block"
-      height={100}
-      width={100}
-    />
+    // <Image
+    //   onClick={() => router.push('/')}
+    //   src="/images/logo.png"
+    //   alt="Logo"
+    //   className="hidden cursor-pointer md:block"
+    //   height={100}
+    //   width={100}
+    // />
+    <Link href="/" className="text-lg font-bold md:text-lg lg:text-3xl">
+      inHome
+    </Link>
   );
 }
 
